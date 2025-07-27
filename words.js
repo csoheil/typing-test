65745a77036d65c19790a8b6ba8fdaf7b3a32a71
soheil mohammadi <soheilmohammadi0o19@gmail.com>
2025-07-26 22:05:26 -0700

    add some word to word.js

--- words.js
+++ words.js
@@ -1,36 +1,189 @@
-// in this file i made array that has a lots of word
-// the usage is in index.html and it is a repository for find a random text
 const words = [
-    'a', 'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit',
-    'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore',
-    'magna', 'aliqua', 'ut', 'enim', 'ad', 'minim', 'veniam', 'quis', 'nostrud',
-    'exercitation', 'ullamco', 'laboris', 'nisi', 'ut', 'aliquip', 'ex', 'ea', 'commodo',
-    'consequat', 'duis', 'aute', 'irure', 'dolor', 'in', 'reprehenderit', 'in', 'voluptate','majid' ,
-    'velit', 'esse', 'cillum', 'dolore', 'eu', 'fugiat', 'nulla', 'pariatur', 'excepteur',
-    'sint', 'occaecat', 'cupidatat', 'non', 'proident', 'sunt', 'in', 'culpa', 'qui',
-    'officia', 'deserunt', 'mollit', 'anim', 'id', 'est', 'laborum', 'hello', 'world',
-    'example', 'text', 'random', 'words', 'generate', 'python', 'code', 'script',
-    'variable', 'function', 'loop', 'condition', 'array', 'object', 'class', 'method',
-    'instance', 'data', 'structure', 'algorithm', 'programming', 'developer',
-    'computer', 'software', 'hardware', 'database', 'query', 'server', 'client',
-    'network', 'protocol', 'internet', 'website', 'browser', 'domain', 'hosting',
-    'email', 'message', 'attachment', 'cloud', 'storage', 'security', 'password',
-    'authentication', 'encryption', 'access', 'control', 'backup', 'restore',
-    'virtual', 'machine', 'container', 'docker', 'kubernetes', 'system', 'interface',
-    'user', 'experience', 'design', 'graphics', 'image', 'video', 'audio', 'file',
-    'document', 'spreadsheet', 'presentation', 'collaboration', 'team', 'project',
-    'management', 'task', 'deadline', 'meeting', 'communication', 'chat', 'forum',
-    'social', 'media', 'platform', 'networking', 'profile', 'settings', 'privacy',
-    'policy', 'terms', 'agreement', 'subscription', 'payment', 'checkout',
-    'cart', 'shopping', 'ecommerce', 'business', 'marketing', 'advertisement',
-    'campaign', 'strategy', 'sales', 'customer', 'feedback', 'support', 'service',
-    'helpdesk', 'ticket', 'resolution', 'quality', 'performance', 'optimization',
-    'analytics', 'report', 'insights', 'dashboard', 'monitoring', 'logging', 'debugging',
-    'testing', 'deployment', 'release', 'version', 'update', 'upgrade', 'patch',
-    'bug', 'fix', 'error', 'issue', 'problem', 'solution', 'learning', 'education',
-    'course', 'tutorial', 'lesson', 'study', 'homework', 'assignment', 'exam', 'grade',
-    'score', 'certificate', 'degree', 'college', 'university', 'research', 'publication',
-    'article', 'book', 'library', 'catalog', 'review', 'rating', 'comment', 'discussion',
-    'community', 'event', 'conference', 'workshop', 'seminar', 'speaker', 'presentation',
-    'session', 'agenda', 'register', 'attendee', 'participant', 'networking', 'opportunity'
+"a" , "about" , "above" , "accept" , "accessory" , "accident" , "accommodation" , "account" , "across" , "act" ,
+
+"action" , "activity" , "add" , "address" , "adult" , "afternoon" , "after" , "again" , "age" , "ago" ,
+
+"agree" , "air" , "airplane" , "airport" , "all" , "allow" , "almost" , "alone" , "along" , "already" ,
+
+"also" , "always" , "am" , "among" , "and" , "animal" , "another" , "answer" , "any" , "anybody" ,
+
+"anyone" , "anything" , "anyway" , "anywhere" , "apartment" , "apple" , "appear" , "area" , "arm" , "around" ,
+
+"arrive" , "art" , "article" , "as" , "ask" , "at" , "attend" , "aunt" , "autumn" , "away" ,
+
+"baby" , "back" , "bad" , "bag" , "ball" , "banana" , "bank" , "bathroom" , "be" , "beach" ,
+
+"beautiful" , "because" , "become" , "bed" , "bedroom" , "before" , "begin" , "behind" , "believe" , "bell" ,
+
+"below" , "belt" , "best" , "better" , "between" , "bicycle" , "big" , "bike" , "bill" , "bird" ,
+
+"birthday" , "black" , "block" , "blue" , "board" , "boat" , "body" , "book" , "boot" , "bottle" ,
+
+"box" , "boy" , "boyfriend" , "brain" , "bread" , "break" , "breakfast" , "bridge" , "bright" , "bring" ,
+
+"brother" , "brown" , "brush" , "build" , "building" , "bus" , "business" , "busy" , "but" , "butter" ,
+
+"button" , "buy" , "by" , "cake" , "call" , "camera" , "camp" , "can" , "car" , "card" ,
+
+"care" , "carry" , "cat" , "catch" , "chair" , "change" , "cheap" , "check" , "cheese" , "chicken" ,
+
+"child" , "children" , "choose" , "city" , "class" , "clean" , "clock" , "close" , "clothes" , "cloud" ,
+
+"coffee" , "cold" , "color" , "come" , "computer" , "cook" , "cool" , "cost" , "count" , "country" ,
+
+"course" , "cover" , "cup" , "cut" , "dance" , "dark" , "daughter" , "day" , "decide" , "deep" ,
+
+"desk" , "dinner" , "do" , "doctor" , "dog" , "door" , "down" , "draw" , "dream" , "dress" ,
+
+"drink" , "drive" , "each" , "early" , "eat" , "egg" , "eight" , "either" , "else" , "end" ,
+
+"enjoy" , "enough" , "even" , "evening" , "every" , "everybody" , "everyone" , "everything" , "example" , "eye" ,
+
+"face" , "family" , "far" , "fast" , "father" , "feel" , "few" , "find" , "fine" , "finish" ,
+
+"fire" , "first" , "fish" , "five" , "floor" , "flower" , "fly" , "food" , "foot" , "for" ,
+
+"forget" , "four" , "free" , "friend" , "from" , "front" , "fruit" , "full" , "fun" , "game" ,
+
+"garden" , "get" , "girl" , "girlfriend" , "give" , "glass" , "go" , "good" , "great" , "green" ,
+
+"group" , "grow" , "hair" , "hand" , "happy" , "hat" , "have" , "he" , "head" , "hear" ,
+
+"heart" , "help" , "her" , "here" , "high" , "him" , "his" , "home" , "hope" , "horse" ,
+
+"hospital" , "hot" , "hotel" , "hour" , "house" , "how" , "hundred" , "I" , "idea" , "if" ,
+
+"in" , "inside" , "into" , "it" , "its" , "job" , "join" , "just" , "keep" , "key" ,
+
+"kitchen" , "know" , "large" , "last" , "late" , "learn" , "leave" , "left" , "leg" , "less" ,
+
+"let" , "letter" , "life" , "light" , "like" , "line" , "listen" , "little" , "live" , "long" ,
+
+"look" , "love" , "make" , "man" , "many" , "market" , "me" , "meet" , "milk" , "minute" ,
+
+"money" , "month" , "more" , "morning" , "mother" , "move" , "movie" , "much" , "music" , "my" ,
+
+"name" , "near" , "new" , "next" , "night" , "nine" , "no" , "not" , "now" , "number" ,
+
+"of" , "off" , "office" , "often" , "old" , "on" , "one" , "only" , "open" , "or" ,
+
+"other" , "our" , "out" , "over" , "own" , "page" , "paper" , "park" , "part" , "party" ,
+
+"pay" , "people" , "person" , "phone" , "picture" , "place" , "plan" , "play" , "please" , "point" ,
+
+"poor" , "put" , "question" , "quick" , "read" , "ready" , "red" , "remember" , "right" , "room" ,
+
+"run" , "said" , "same" , "say" , "school" , "see" , "sell" , "send" , "set" , "seven" ,
+
+"she" , "shop" , "short" , "show" , "sing" , "sister" , "sit" , "six" , "sleep" , "small" ,
+
+"some" , "something" , "son" , "soon" , "sorry" , "sound" , "speak" , "stand" , "start" , "stay" ,
+
+"stop" , "store" , "story" , "street" , "strong" , "study" , "sun" , "table" , "take" , "talk" ,
+
+"teacher" , "tell" , "ten" , "than" , "thank" , "that" , "the" , "their" , "them" , "then" ,
+
+"there" , "these" , "they" , "thing" , "think" , "this" , "those" , "three" , "time" , "to" ,
+
+"today" , "together" , "too" , "top" , "town" , "tree" , "try" , "turn" , "two" , "under" ,
+
+"understand" , "up" , "us" , "use" , "very" , "visit" , "wait" , "walk" , "want" , "watch" ,
+
+"water" , "way" , "we" , "week" , "well" , "what" , "when" , "where" , "which" , "white" ,
+
+"who" , "why" , "will" , "with" , "woman" , "work" , "world" , "write" , "year" , "yellow" ,
+
+"yes" , "you" , "young" , "your" , "zero" , "above" , "accept" , "across" , "act" , "add" ,
+
+"address" , "agree" , "air" , "all" , "almost" , "alone" , "along" , "already" , "also" , "always" ,
+
+"am" , "and" , "another" , "answer" , "any" , "anyone" , "anything" , "anywhere" , "apple" , "area" ,
+
+"arm" , "around" , "arrive" , "ask" , "at" , "aunt" , "away" , "baby" , "back" , "bad" ,
+
+"bag" , "ball" , "bank" , "bathroom" , "be" , "beach" , "beautiful" , "because" , "bed" , "bedroom" ,
+
+"before" , "begin" , "behind" , "believe" , "below" , "best" , "better" , "between" , "big" , "bike" ,
+
+"bird" , "birthday" , "black" , "blue" , "board" , "boat" , "body" , "book" , "bottle" , "box" ,
+
+"boy" , "bread" , "break" , "breakfast" , "bridge" , "bright" , "bring" , "brother" , "brown" , "build" ,
+
+"building" , "bus" , "business" , "busy" , "but" , "buy" , "by" , "cake" , "call" , "camera" ,
+
+"can" , "car" , "card" , "care" , "carry" , "cat" , "chair" , "change" , "cheap" , "check" ,
+
+"cheese" , "child" , "children" , "city" , "class" , "clean" , "clock" , "close" , "clothes" , "cloud" ,
+
+"coffee" , "cold" , "color" , "come" , "computer" , "cook" , "cool" , "cost" , "count" , "country" ,
+
+"course" , "cover" , "cup" , "cut" , "dance" , "dark" , "daughter" , "day" , "decide" , "deep" ,
+
+"desk" , "dinner" , "do" , "doctor" , "dog" , "door" , "down" , "draw" , "dream" , "dress" ,
+
+"drink" , "drive" , "each" , "early" , "eat" , "egg" , "eight" , "end" , "enjoy" , "enough" ,
+
+"even" , "evening" , "every" , "everyone" , "everything" , "example" , "eye" , "face" , "family" , "far" ,
+
+"fast" , "father" , "feel" , "few" , "find" , "fine" , "finish" , "fire" , "first" , "fish" ,
+
+"five" , "floor" , "flower" , "fly" , "food" , "foot" , "for" , "forget" , "four" , "free" ,
+
+"friend" , "from" , "front" , "fruit" , "full" , "fun" , "game" , "garden" , "get" , "girl" ,
+
+"give" , "glass" , "go" , "good" , "great" , "green" , "group" , "grow" , "hair" , "hand" ,
+
+"happy" , "hat" , "have" , "he" , "head" , "hear" , "heart" , "help" , "her" , "here" ,
+
+"high" , "him" , "his" , "home" , "hope" , "horse" , "hospital" , "hot" , "hotel" , "hour" ,
+
+"house" , "how" , "hundred" , "I" , "idea" , "if" , "in" , "inside" , "into" , "it" ,
+
+"its" , "job" , "join" , "just" , "keep" , "key" , "kitchen" , "know" , "large" , "last" ,
+
+"late" , "learn" , "leave" , "left" , "leg" , "less" , "let" , "letter" , "life" , "light" ,
+
+"like" , "line" , "listen" , "little" , "live" , "long" , "look" , "love" , "make" , "man" ,
+
+"many" , "market" , "me" , "meet" , "milk" , "minute" , "money" , "month" , "more" , "morning" ,
+
+"mother" , "move" , "movie" , "much" , "music" , "my" , "name" , "near" , "new" , "next" ,
+
+"night" , "nine" , "no" , "not" , "now" , "number" , "of" , "off" , "office" , "often" ,
+
+"old" , "on" , "one" , "only" , "open" , "or" , "other" , "our" , "out" , "over" ,
+
+"own" , "page" , "paper" , "park" , "part" , "party" , "pay" , "people" , "person" , "phone" ,
+
+"picture" , "place" , "plan" , "play" , "please" , "point" , "poor" , "put" , "question" , "quick" ,
+
+"read" , "ready" , "red" , "remember" , "right" , "room" , "run" , "said" , "same" , "say" ,
+
+"school" , "see" , "sell" , "send" , "set" , "seven" , "she" , "shop" , "short" , "show" ,
+
+"sing" , "sister" , "sit" , "six" , "sleep" , "small" , "some" , "something" , "son" , "soon" ,
+
+"sorry" , "sound" , "speak" , "stand" , "start" , "stay" , "stop" , "store" , "story" , "street" ,
+
+"strong" , "study" , "sun" , "table" , "take" , "talk" , "teacher" , "tell" , "ten" , "than" ,
+
+"thank" , "that" , "the" , "their" , "them" , "then" , "there" , "these" , "they" , "thing" ,
+
+"think" , "this" , "those" , "three" , "time" , "to" , "today" , "together" , "too" , "top" ,
+
+"town" , "tree" , "try" , "turn" , "two" , "under" , "understand" , "up" , "us" , "use" ,
+
+"very" , "visit" , "wait" , "walk" , "want" , "watch" , "water" , "way" , "we" , "week" ,
+
+"well" , "what" , "when" , "where" , "which" , "white" , "who" , "why" , "will" , "with" ,
+
+"woman" , "work" , "world" , "write" , "year" , "yellow" , "yes" , "you" , "young" , "your" ,
+
+"zero" , "a" , "about" , "above" , "accept" , "accessory" , "accident" , "accommodation" , "account" , "across" ,
+
+"act" , "action" , "activity" , "add" , "address" , "adult" , "afternoon" , "after" , "again" , "age" ,
+
+"ago" , "agree" , "air" , "airplane" , "airport" , "all" , "allow" , "almost" , "alone" , "along" ,
+
+"already" , "also" , "always" , "am" , "among" , "and" , "animal" , "another" , "answer" , "any"
 ];

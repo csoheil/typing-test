6868ca81ee5caded769addf5879564116034a373
soheil mohammadi <soheilmohammadi0o19@gmail.com>
2025-08-15 10:30:42 -0700

    edit about.php

--- about.php
+++ about.php
@@ -15,8 +15,7 @@ if (!isset($_SESSION['user_id'])) {
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-
-<!--    style-->
+    
 
     <style>
         * {
